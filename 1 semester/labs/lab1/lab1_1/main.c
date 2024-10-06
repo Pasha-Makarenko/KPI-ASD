4f7dddbdf54b6f36571db5b8b4f58988c1b8bf13
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-10-06 20:08:08 +0300

    Update main.c

--- 1 semester/labs/lab1/lab1_1/main.c
+++ 1 semester/labs/lab1/lab1_1/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+double static func_1(double x) {
+    return -6 * x * x + 8;
+}
+
+double static func_2(double x) {
+    return -1 * x * x * x / 7 + 10;
+}
+
+int main(void) {
+    double argument;
+    
+    printf("Enter value: ");
+    scanf("%lf", &argument);
+
+    if (argument >= 0) {
+        if (argument < 7) {
+            printf("Result: %lf", func_1(argument));
+        } else if (argument >= 11) {
+            printf("Result: %lf", func_2(argument));
+        } else {
+            printf("No value");
+        }
+    } else if (argument <= -10) {
+        printf("Result: %lf", func_2(argument));
+    } else {
+        printf("No value");
+    }
+
+    return 0;
+}
