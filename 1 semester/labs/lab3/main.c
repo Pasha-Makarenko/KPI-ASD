4078e83e521fd63834619c67a0bf58200b191fe7
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-10-22 20:27:26 +0300

    Lab 3: update code

--- 1 semester/labs/lab3/main.c
+++ 1 semester/labs/lab3/main.c
@@ -2,8 +2,43 @@
 
 #define print_line() printf("=========================================================================\n")
 
+struct coords {
+  short row;
+  short column;
+};
+
+void init_matrix(const short rows, const short columns, int matrix[rows][columns]) {
+  printf("Input elements of matrix:\n");
+  print_line();
+
+  for (short row = 0; row < rows; row++) {
+    for (short column = 0; column < columns; column++) {
+      printf("Input element (%hu; %hu):", row + 1, column + 1);
+      scanf("%d", &matrix[row][column]);
+    }
+    print_line();
+  }
+}
+
+int find_element_pos(const short rows, const short columns, int matrix[rows][columns], const int target_value, struct coords *pos) {
+  printf("Start search...\n");
+  print_line();
+
+  for (short column = 0; column < rows; column++) {
+    for (short row = 0; row < columns; row++) {
+      if (matrix[row][column] == target_value) {
+        pos->row = row + 1;
+        pos->column = column + 1;
+        return 0;
+      }
+    }
+  }
+
+  return -1;
+}
+
 int main(void) {
-  unsigned short rows_count, columns_count;
+  short rows_count, columns_count;
   int target_value;
 
   print_line();
@@ -15,38 +50,27 @@ int main(void) {
 
   if (rows_count <= 0 || columns_count <= 0) {
     printf("Sizes cannot be less then or equal 0");
-    return 0;
+    return -1;
   }
 
   int matrix[rows_count][columns_count];
-
-  printf("Input elements of matrix:\n");
-  print_line();
-
-  for (unsigned short row = 0; row < rows_count; row++) {
-    for (unsigned short column = 0; column < columns_count; column++) {
-      printf("Input element (%hu; %hu):", row + 1, column + 1);
-      scanf("%d", &matrix[row][column]);
-    }
-    print_line();
-  }
+  init_matrix(rows_count, columns_count, matrix);
 
   printf("Input target value:");
   scanf("%d", &target_value);
 
-  printf("Start search...\n");
-  print_line();
+  struct coords pos;
+  const int status = find_element_pos(rows_count, columns_count, matrix, target_value, &pos);
 
-  for (unsigned short column = 0; column < columns_count; column++) {
-    for (unsigned short row = 0; row < rows_count; row++) {
-      if (matrix[row][column] == target_value) {
-        printf("(%d; %d)\n", row + 1, column + 1);
-      }
-    }
+  switch (status) {
+    case 0:
+      printf("Coords: (%d; %d)\n", pos.row, pos.column);
+      break;
+    case -1:
+      printf("Target element not found");
+      break;
+    default: ;
   }
 
-  print_line();
-  printf("Done\n");
-
-  return 0;
+  return status;
 }
